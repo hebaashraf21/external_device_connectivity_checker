8c2dce6e2542a31129f5c20d461960f92bb4d501
hebaashraf21 <heba.elmaaboud00@eng-st.cu.edu.eg>
2023-06-05 05:12:57 +0300

    (fix): project name

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
+          title: 'Connectivity checker',
           theme: ThemeData(
             appBarTheme: AppBarTheme(
                 systemOverlayStyle: SystemUiOverlayStyle(
